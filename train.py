0bf4ebe88c16586cffd1c696a1e36e7eefe18538
cliedl <christian.liedl@gmx.net>
2024-03-11 14:44:07 +0100

    Added simple training script (PPO)

---
+++ train.py
@@ -0,0 +1,48 @@
+from environment import QubitEnv
+from plot_utils import render_episode
+
+import ray
+from ray import tune
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.tune.registry import register_env
+
+from tqdm.auto import tqdm
+
+
+def env_creator(env_config):
+    return QubitEnv()  # return an instance of your environment
+
+
+# Replace 'QubitEnv-v0' with your desired environment ID
+register_env('QubitEnv-v0', env_creator)
+
+
+ray.init()  # Initialize Ray; include necessary arguments based on your setup
+
+
+algo = (
+    PPOConfig()
+    .rollouts(num_rollout_workers=1)
+    .resources(num_gpus=0)
+    .environment(env="QubitEnv-v0")
+    .build()
+)
+
+for i in tqdm(range(500)):
+    result = algo.train()
+    print(f"Mean episode reward: {result['episode_reward_mean']}")
+
+    if i % 5 == 0:
+        checkpoint_dir = algo.save().checkpoint.path
+        print(f"Checkpoint saved in directory {checkpoint_dir}")
+
+
+states_episode = []
+env = QubitEnv()
+terminated = False
+while not terminated:
+    action = algo.compute_single_action(env.state)
+    state, reward, terminated, truncated, info = env.step(action)
+    states_episode.append(state)
+
+render_episode(states_episode, delay=0.01)
